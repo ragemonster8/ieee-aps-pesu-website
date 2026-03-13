5fc6bafcb22f096bb98220030f0dd95606d8c764
Aryan Adoni <aryanadoni8@gmail.com>
2026-03-14 00:33:37 +0530

    Add Navbar and Footer components

---
+++ website/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white text-center p-4 mt-10">
+      <p>© {new Date().getFullYear()} IEEE AP-S PES University</p>
+    </footer>
+  );
+}
